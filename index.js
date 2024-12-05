ab40009ef5040e416fc200d7745e585ac5b6d2b2
luqmanshof <luqmanshof@gmail.com>
2024-12-05 11:53:30 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(getPosts());
 
 console.log(`Posts Length: ${getPostsLength()}`);
 
+
 // const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
 
 // console.log(`Random Number: ${generateRandomNumber()}`);
